4bd8b22df25e9289ab63d824bec01d3ef887669a
Skips-Scramble <95422872+Skips-Scramble@users.noreply.github.com>
2023-07-04 17:45:35 -0400

    Remove assumptions info from in-line to standalone file
    Do this as this file will now live on its own.

--- stochastic_finances.py
+++ stochastic_finances.py
@@ -1,19 +1,8 @@
+import json
+
 import numpy_financial as npf
 
-# Initial assumptions
-financial_assumptions = {
-    "birthday": 12 - 22 - 1986,
-    "retirement_age_yrs": 60,
-    "retirement_age_months": 0,
-    "mean_interest_per_yr": 5.0,
-    "mean_inflation_per_yr": 3.0,
-    "ss_withdraw_age_yrs": 70,
-    "ss_withdraw_age_mos": 0,
-    "ss_withdraw_amt": 0,
-    "current_monthly_bills": 1500,
-    "current_monthly_rent": 1100,
-    "rent_end_age_yrs": 70,
-    "rent_end_age_months": 0,
-}
+with open('input_assumptions.json') as json_data:
+    data = json.load(json_data)
 
-# ss amount withdrawn = optional
+
