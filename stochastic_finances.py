8e319c30484103c964643f6d0b6a50ecde45746c
Skips-Scramble <95422872+Skips-Scramble@users.noreply.github.com>
2023-07-04 16:12:40 -0400

    Create stochastic_finances.py
    Start the main script

---
+++ stochastic_finances.py
@@ -0,0 +1,19 @@
+import numpy_financial as npf
+
+# Initial assumptions
+financial_assumptions = {
+    "birthday": 12 - 22 - 1986,
+    "retirement_age_yrs": 60,
+    "retirement_age_months": 0,
+    "mean_interest_per_yr": 5.0,
+    "mean_inflation_per_yr": 3.0,
+    "ss_withdraw_age_yrs": 70,
+    "ss_withdraw_age_mos": 0,
+    "ss_withdraw_amt": 0,
+    "current_monthly_bills": 1500,
+    "current_monthly_rent": 1100,
+    "rent_end_age_yrs": 70,
+    "rent_end_age_months": 0,
+}
+
+# ss amount withdrawn = optional
