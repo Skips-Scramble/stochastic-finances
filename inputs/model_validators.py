4e6306b4090b7fcda02466ba50621297f4c91367
Skips-Scramble <95422872+Skips-Scramble@users.noreply.github.com>
2024-02-17 13:25:45 -0500

    Get validations set up
    Do this to provide useful feedback to users.

--- inputs/model_validators.py
+++ inputs/model_validators.py
@@ -3,6 +3,20 @@ from datetime import date
 from django.core.exceptions import ValidationError
 
 
+def decimal_validator(input: float):
+    """Ensure there are at most 2 decimal point used"""
+    if round(input, 2) != input:
+        raise ValidationError("Please only use up to 2 decimal points")
+
+
+def validate_range_age_mos(input: int):
+    """Custom validation to ensure an input is between two values"""
+    if input < 0 or input > 11:
+        raise ValidationError(
+            "Please enter a value between 0 (the month you were born) and 11"
+        )
+
+
 def validate_range_birthdate(input: date):
     """Custom validation of the user's birthdate"""
     if input.year < 1900:
@@ -15,19 +29,11 @@ def validate_range_age_yrs(input: int):
         raise ValidationError("Please enter a value between 0 and 110")
 
 
-def validate_range_age_mos(input: int):
-    """Custom validation to ensure an input is between two values"""
-    if input < 0 or input > 11:
-        raise ValidationError(
-            "Please enter a value between 0 (the month you were born) and 11"
-        )
-
-
 def validate_base_savings(input: float):
     """Ensure base_savings is reasonable"""
-    if input < -10_000_000 or input > 10_000_000:
+    if input < -100_000_000 or input > 100_000_000:
         raise ValidationError(
-            "That's quite a lot of savings. Try to keep in under $10,000,000, plus or minus"
+            "That's quite a lot of savings. Try to keep it within $100,000,000, plus or minus"
         )
 
 
@@ -35,7 +41,7 @@ def validate_base_saved_per_mo(input: float):
     """Ensure base_saved_per_mo is reasonable"""
     if input < -10_000_000 or input > 10_000_000:
         raise ValidationError(
-            "That's quite a lot of savings. Try to keep in under $10,000,000, plus or minus"
+            "That's quite a lot of savings. Try to keep in within $10,000,000, plus or minus"
         )
 
 
@@ -43,5 +49,83 @@ def validate_base_per_yr_increase(input: float):
     """Ensure base_savings_per_yr_increase is reasonable"""
     if input < -1000 or input > 1000:
         raise ValidationError(
-            "That's quite a high year-over-year savings increase. Try to keep in under 1,000%, plus or minus"
+            "That's quite a high year-over-year savings increase. Try to keep in within 1,000%, plus or minus"
         )
+
+
+def validate_savings_lower_limit(input: float):
+    """Ensure savings lower limit is sane"""
+    if input > 100_000_000:
+        raise ValidationError("Please lower limit to under $100,000,000")
+
+
+def validate_base_monthly_bills(input: float):
+    """Ensure monthly bills are reasonable"""
+    if input < -10_000_000 or input > 10_000_000:
+        raise ValidationError(
+            "Please keep cash flows within plus or minus $10,000,000 a month"
+        )
+
+
+def validate_pmt_start_age_yrs(input: int):
+    """Ensure payments start at a reasonable age"""
+    if input < 0:
+        raise ValidationError("Please ensure the age is at least 0 years")
+
+
+def validate_pmt_length_yrs(input: float):
+    """Ensure payment lengths (in years) is reasonable"""
+    if input < 0 or input > 110:
+        raise ValidationError("Please enter a value between 0 and 110")
+
+
+def validate_down_pmt(input: float):
+    """Ensure down payments are reasonable"""
+    if input < -100_000_000 or input > 100_000_000:
+        raise ValidationError(
+            "Hey big spender, please keep down payments to less than $100,000,000 in absolute value"
+        )
+
+
+def validate_monthly_pmt(input: float):
+    """Ensure down payments are reasonable"""
+    if input < -100_000_000 or input > 100_000_000:
+        raise ValidationError(
+            "Hey big spender, please keep monthly payments to less than $100,000,000 in absolute value"
+        )
+
+
+def validate_base_retirement(input: float):
+    """Validate base retirement amount"""
+    if input < -100_000_000 or input > 100_000_000:
+        raise ValidationError(
+            "That's quite a lot of retirement. Try to keep it within $100,000,000, plus or minus"
+        )
+
+
+def validate_base_retirement_per_mo(input: float):
+    """Valide the base_retirement_per_mo input"""
+    if input < -1_000_000 or input > 1_000_000:
+        raise ValidationError("Please enter a value within $1,000,000, plus or minus")
+
+
+def validate_base_retirement_per_yr_increase(input: float):
+    """Validate base_retirement_per_yr_increase input"""
+    if input < -1_000_000 or input > 1_000_000:
+        raise ValidationError(
+            "That looks like a big retirement swing. Please keep values to within $1,000,000, plus or minus"
+        )
+
+
+def validate_retirement_extra_expenses(input: float):
+    """Validate retirement extra expenses"""
+    limit = 10_000_000
+    if not (-1) * limit < input < limit:
+        raise ValidationError(f"Please keep values within ${limit}, plus or minus")
+
+
+def validate_rates_per_yr(input: float):
+    """Validate base_rf_interest_per_yr"""
+    limit = 100
+    if not (-1) * limit * 100 < input < limit * 100:
+        raise ValidationError(f"Please keep values within ${limit}, plus or minus")
--- inputs/models.py
+++ inputs/models.py
@@ -1,16 +1,25 @@
-from datetime import date
-
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.db import models
 
 from .model_validators import (
+    decimal_validator,
+    validate_base_monthly_bills,
     validate_base_per_yr_increase,
+    validate_base_retirement,
+    validate_base_retirement_per_mo,
+    validate_base_retirement_per_yr_increase,
     validate_base_saved_per_mo,
     validate_base_savings,
+    validate_down_pmt,
+    validate_monthly_pmt,
+    validate_pmt_length_yrs,
+    validate_pmt_start_age_yrs,
     validate_range_age_mos,
     validate_range_age_yrs,
     validate_range_birthdate,
+    validate_rates_per_yr,
+    validate_retirement_extra_expenses,
+    validate_savings_lower_limit,
 )
 
 
@@ -43,22 +52,22 @@ class GeneralInputsModel(models.Model):
 class SavingsInputsModel(models.Model):
     is_active = models.BooleanField(
         default=False,
-        help_text="This determines whether or not these inputs will be used to calculate your financial situation",
     )
     base_savings = models.FloatField(
-        validators=[validate_base_savings],
-        help_text='This is the amount of money you save each month and contribute to your savings account(s). A negative amount would represent you are spending that amount each month. If this is the case, it would probably be better to put a 0 in this field and move those payments to the "Payemnts" section',
+        validators=[validate_base_savings, decimal_validator]
     )
     base_saved_per_mo = models.FloatField(
-        validators=[validate_base_saved_per_mo],
-        help_text="This is the average amount of money you save per month. Said another way, this is the average amount your savings account increases by each month (excluding interest). This could be negative.",
+        validators=[validate_base_saved_per_mo, decimal_validator],
     )
     base_savings_per_yr_increase = models.FloatField(
-        validators=[validate_base_per_yr_increase],
-        help_text="The year-over-year savings increase (as a percent). For example, if you expect a 3% salary increase every year, you might put 3% here.",
+        validators=[validate_base_per_yr_increase, decimal_validator],
     )
-    savings_lower_limit = models.FloatField()
-    base_monthly_bills = models.FloatField(null=True, blank=True)
+    savings_lower_limit = models.FloatField(
+        validators=[validate_savings_lower_limit, decimal_validator]
+    )
+    base_monthly_bills = models.FloatField(
+        validators=[validate_base_monthly_bills, decimal_validator]
+    )
     created_by = models.ForeignKey(
         User,
         related_name="savings_inputs",
@@ -73,12 +82,24 @@ class SavingsInputsModel(models.Model):
 class PaymentsInputsModel(models.Model):
     is_active = models.BooleanField(default=False)
     pmt_name = models.CharField(max_length=100, null=True, blank=True)
-    pmt_start_age_yrs = models.IntegerField(null=True, blank=True)
-    pmt_start_age_mos = models.IntegerField(null=True, blank=True)
-    pmt_length_yrs = models.IntegerField(null=True, blank=True)
-    pmt_length_mos = models.IntegerField(null=True, blank=True)
-    down_pmt = models.FloatField(null=True, blank=True)
-    monthly_pmt = models.FloatField(null=True, blank=True)
+    pmt_start_age_yrs = models.IntegerField(
+        null=True, blank=True, validators=[validate_pmt_start_age_yrs]
+    )
+    pmt_start_age_mos = models.IntegerField(
+        null=True, blank=True, validators=[validate_range_age_mos]
+    )
+    pmt_length_yrs = models.IntegerField(
+        null=True, blank=True, validators=[validate_pmt_length_yrs]
+    )
+    pmt_length_mos = models.IntegerField(
+        null=True, blank=True, validators=[validate_range_age_mos]
+    )
+    down_pmt = models.FloatField(
+        null=True, blank=True, validators=[validate_down_pmt, decimal_validator]
+    )
+    monthly_pmt = models.FloatField(
+        null=True, blank=True, validators=[validate_monthly_pmt, decimal_validator]
+    )
     created_by = models.ForeignKey(
         User,
         related_name="payments_inputs",
@@ -94,10 +115,18 @@ class PaymentsInputsModel(models.Model):
 
 class RetirementInputsModel(models.Model):
     is_active = models.BooleanField(default=False)
-    base_retirement = models.FloatField()
-    base_retirement_per_mo = models.FloatField()
-    base_retirement_per_yr_increase = models.FloatField()
-    retirement_extra_expenses = models.FloatField()
+    base_retirement = models.FloatField(
+        validators=[validate_base_retirement, decimal_validator]
+    )
+    base_retirement_per_mo = models.FloatField(
+        validators=[validate_base_retirement_per_mo, decimal_validator]
+    )
+    base_retirement_per_yr_increase = models.FloatField(
+        validators=[validate_base_retirement_per_yr_increase, decimal_validator]
+    )
+    retirement_extra_expenses = models.FloatField(
+        validators=[validate_retirement_extra_expenses, decimal_validator]
+    )
     created_by = models.ForeignKey(
         User,
         related_name="retirement_inputs",
@@ -113,9 +142,15 @@ class RetirementInputsModel(models.Model):
 
 class RatesInputsModel(models.Model):
     is_active = models.BooleanField(default=False)
-    base_rf_interest_per_yr = models.FloatField()
-    base_mkt_interest_per_yr = models.FloatField()
-    base_inflation_per_yr = models.FloatField()
+    base_rf_interest_per_yr = models.FloatField(
+        validators=[validate_rates_per_yr, decimal_validator]
+    )
+    base_mkt_interest_per_yr = models.FloatField(
+        validators=[validate_rates_per_yr, decimal_validator]
+    )
+    base_inflation_per_yr = models.FloatField(
+        validators=[validate_rates_per_yr, decimal_validator]
+    )
     created_by = models.ForeignKey(
         User,
         related_name="rates_inputs",
